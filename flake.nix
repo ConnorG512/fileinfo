fceb4b5f447a47e9934077ec4fd96f0ea094d65a
Connor Garey <connorgarey1999@gmail.com>
2025-08-22 01:57:02 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "File Info Nix flake.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in 
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        zig
+        lldb
+      ];
+    };
+
+    shellHook = ''
+    echo "Entering shell..."
+    '';
+  };
+}
