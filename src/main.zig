5cd583acc3de739948a3f69fd24cf54c80a60317
Connor Garey <connorgarey1999@gmail.com>
2025-08-22 21:07:06 +0100

    Added output to main

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
+
 const CommandParser = @import("command-parser.zig").CommandParser;
 const ActivatedFlags = @import("command-parser.zig").ActivatedFlags;
+const FlagList = @import("command-parser.zig").AvailableFlags;
 const StdOutWriter = @import("stdout-writer.zig").StdOutWriter;
 
 pub fn main() !void {
@@ -8,19 +10,27 @@ pub fn main() !void {
     CommandParser.calculateArgumentNum() catch |err| {
         switch (err) {
             error.ReachedMaxArgCount => {
+                try StdOutWriter.writeMessage("Too many flags provided!");
                 return err;
             },
             error.TooLittleArgumentsProvided => {
+                try StdOutWriter.writeMessage("Not Enough flags provided! See \"--flags\" for options.");
                 return err;
             },
-            error.UnknownFlag => {
-                return err;
-            }
+            else => {},
         }
     };
 
     var activated_flags: ActivatedFlags = .{};
-    CommandParser.parseCommandFlags(&activated_flags) catch {
-        try StdOutWriter.writeMessage("Unknown flag! Please see --flags");
-    };
+    CommandParser.parseCommandFlags(&activated_flags);
+
+    // Execute commands.
+    if (activated_flags.help == 1) {
+        const flag_list: FlagList = .{};
+
+        try StdOutWriter.writeMessage("Available commands:");
+        try StdOutWriter.writeMessage(flag_list.help);
+        try StdOutWriter.writeMessage(flag_list.help_short);
+        try StdOutWriter.writeMessage(flag_list.version);
+    }
 }
