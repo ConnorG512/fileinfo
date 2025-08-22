8684dee9e9003f134f3eccdd7c076e585af105bc
Connor Garey <connorgarey1999@gmail.com>
2025-08-22 02:58:15 +0100

    Started work on command parser

---
+++ src/command-parser.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+
+const ParserError = error {
+    ReachedMaxArgCount,
+    TooLittleArgumentsProvided,
+};
+
+pub const CommandParser = struct {
+    pub fn calculateArgumentNum() ParserError!void {
+        const argument_count: u8 = @intCast(std.os.argv.len);
+        std.log.debug("(calculateArgumentNum) Command number: {d}", .{argument_count});
+
+        const max_argument_count: u8 = comptime 12;
+        if (argument_count > max_argument_count) {
+            return error.ReachedMaxArgCount;
+        }
+        
+        const minimum_argument_count = comptime 1;
+        if (argument_count <= minimum_argument_count) {
+            return error.TooLittleArgumentsProvided;
+        }
+    }
+
+    pub fn parseCommandFlags() void {
+        for (std.os.argv) |flag| {
+            if (std.mem.eql(u8, std.mem.sliceTo(flag, 0), "--help")) {
+                std.log.debug("Help Called!", .{});
+            }
+            if (std.mem.eql(u8, std.mem.sliceTo(flag, 0), "-h")) {
+                std.log.debug("Help Called!", .{});
+            }
+        }
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,17 @@
+const CommandParser = @import("command-parser.zig").CommandParser;
 
 pub fn main() !void {
+
+    CommandParser.calculateArgumentNum() catch |err| {
+        switch (err) {
+            error.ReachedMaxArgCount => {
+                return err;
+            },
+            error.TooLittleArgumentsProvided => {
+                return err;
+            }
+        }
+    };
+
+    CommandParser.parseCommandFlags();
 }
