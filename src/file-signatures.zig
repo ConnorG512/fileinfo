03e2a115e3a06521cee6b6a44dd4e2fb2e01c13c
Connor Garey <connorgarey1999@gmail.com>
2025-08-23 21:59:15 +0100

    Added pub to file signatures so they can be accessed

--- src/file-signatures.zig
+++ src/file-signatures.zig
@@ -19,20 +19,20 @@ fn createFileSignature(comptime signature: []const u8, comptime name: []const u8
 }
 
 // Base:
-const unknown_format: FileSignature = createFileSignature(
+pub const unknown_format: FileSignature = createFileSignature(
     &[_]u8{ 0xAA, 0xAA, 0xAA, 0xAA, 0xAA }, 
     "Unknown");
 
 // Image format:
-const png_file: FileSignature = createFileSignature(
+pub const png_file: FileSignature = createFileSignature(
     &[_]u8{ 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A }, 
     "PNG (Portable Network Graphics)" );
 
-const jpeg_2000: FileSignature = createFileSignature(
+pub const jpeg_2000: FileSignature = createFileSignature(
     &[_]u8{ 0x00, 0x00, 0x00, 0x0C, 0x6A, 0x50, 0x20, 0x20, 0x0D, 0x0A, 0x87, 0x0A }, 
     "JPEG 2000");
 
 // Video format:
-const mpeg4_iso: FileSignature = createFileSignature(
+pub const mpeg4_iso: FileSignature = createFileSignature(
     &[_]u8{ 0x66, 0x74, 0x79, 0x70, 0x69, 0x73, 0x6F, 0x6D }, 
     "ISO Base Media File (MPEG-4)");
