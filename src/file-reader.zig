49450b080d843c85e1e74eff0b335ae3f3e28c23
Connor Garey <connorgarey1999@gmail.com>
2025-08-23 22:11:36 +0100

    Moved buffer to the calling function so it can also be used in determine

--- src/file-reader.zig
+++ src/file-reader.zig
@@ -4,17 +4,20 @@ const FileSignatures = @import("file-signatures.zig");
 pub const FileOpener = struct {
 
     pub fn scanForFileSignature() !void {
+        var file_buffer: [32]u8 = undefined; 
         const file_descriptor = openFile();
-        try readFileSignatureBytes(&file_descriptor);
+        try readFileSignatureBytes(&file_descriptor, &file_buffer);
     }
     fn openFile() usize {
         const file_descriptor: usize = std.os.linux.open(std.os.argv[1], .{ .ACCMODE = .RDONLY }, 0);
         return file_descriptor;
     }
-    fn readFileSignatureBytes(file_descriptor: *const usize) !void {
-        var file_buffer: [32]u8 = undefined; 
-        const num_bytes_read = std.os.linux.read(@intCast(file_descriptor.*), &file_buffer, file_buffer.len);
-        std.log.debug("{X}", .{file_buffer[0..]});
+    fn readFileSignatureBytes(file_descriptor: *const usize, read_buffer: []u8) !void {
+        const num_bytes_read = std.os.linux.read(@intCast(file_descriptor.*), read_buffer.ptr, read_buffer.len);
+        std.log.debug("{X}", .{read_buffer[0..]});
         std.log.debug("Num bytes read: {d}.", .{num_bytes_read});
+    }
+    fn determineFileSignature() void {
+
     }
 };
