7a8ed18cd15429fcc8ffd4dc6822246ba5f62353
Connor Garey <connorgarey1999@gmail.com>
2025-08-23 22:11:22 +0100

    Added ELF file signature

--- src/file-signatures.zig
+++ src/file-signatures.zig
@@ -26,7 +26,7 @@ pub const unknown_format: FileSignature = createFileSignature(
 // Image format:
 pub const png_file: FileSignature = createFileSignature(
     &[_]u8{ 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A }, 
-    "PNG (Portable Network Graphics)" );
+    "PNG (Portable Network Graphics)");
 
 pub const jpeg_2000: FileSignature = createFileSignature(
     &[_]u8{ 0x00, 0x00, 0x00, 0x0C, 0x6A, 0x50, 0x20, 0x20, 0x0D, 0x0A, 0x87, 0x0A }, 
@@ -36,3 +36,8 @@ pub const jpeg_2000: FileSignature = createFileSignature(
 pub const mpeg4_iso: FileSignature = createFileSignature(
     &[_]u8{ 0x66, 0x74, 0x79, 0x70, 0x69, 0x73, 0x6F, 0x6D }, 
     "ISO Base Media File (MPEG-4)");
+
+// Executables:
+pub const elf: FileSignature = createFileSignature(
+    &[_]u8{ 0x7F, 0x45, 0x4C, 0x46 }, 
+    "ELF Executable");
